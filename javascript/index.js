356da3c7ed3bd0548214c1434861a2203384a12d
Dmcgeough <danny.mcgeough@gmail.com>
2018-06-18 16:15:33 -0400

    added script tags and a new function

--- javascript/index.js
+++ javascript/index.js
@@ -4,8 +4,10 @@ function buyFunction() {
 }
 
 function addToCart() {
-    
+    var t = bookArray[i].title;
+    var p = bookArray[i].price;
 }
+
 
 
 // create an array that isn't hard coded with values that can be changed with above function to be true or false which in turn
@@ -14,21 +16,20 @@ function addToCart() {
 // inject that into a table in order to show the book title and its price in the shopping cart page after it is clicked on in
 // the store page. A final bit of javascript that it would be helpful to add would be a loop of some sort to add up all of the 
 // books 'bought' and to figure out a way to add them all up and put them into a total price table data element on the shopping
-// cart page.
+// cart page. The function to add up price would be fairly straight forward. However, the function that would require me to
+//pull values from an Array and then inject those values using JavaScript into a different HTML element is going to be tough.
+//var book = [title, price];
+var book0 = ["The Girl with the Dragon Tattoo", 7.99 ];
+var book1 = ["Never Go Back", 7.99];
+var book2 = ["Best Served Cold", 13.99];
+var book3 = ["Cronkite", 19.99];
+var book4 = ["50 Shades of Grey", 9.99];
+var book5 = ["The President is Missing", 17.99];
+var book6 = ["Shelter in Place", 14.99];
+var book7 = ["The Soul of America", 17.99];
+var book8 = ["The Fallen", 14.99];
+var book9 = ["Something in the Water", 13.99];
+var book10 = ["The Grey Ghost", 13.99];
+var book11 = ["The Death of Mrs. Westaway", 12.99];
 
-var book = [title, author, price];
-var book1 = ["The Girl with the Dragon Tattoo", "Stieg Larsson", 7.99 ];
-var book2 = ["Never Go Back", "Lee Child", 7.99];
-var book3 = ["Best Served Cold", "Joe Abercrombie", 13.99];
-var book4 = ["Cronkite", "Douglas Brinkley", 19.99];
-var book5 = ["50 Shades of Grey", "E. L. James", 9.99];
-var book6 = ["The President is Missing", "Bill Clinton and James Patterson", 17.99];
-var book7 = ["Shelter in Place", "Nora Roberts", 14.99];
-var book8 = ["The Soul of America", "Jon Meacham", 17.99];
-var book9 = ["The Fallen", "David Baldacci", 14.99];
-var book10 = ["Something in the Water", "Catherine Steadman", 13.99];
-var book11 = ["The Grey Ghost", "Clive Cussler", 13.99];
-var book12 = ["The Death of Mrs. Westaway", "Ruth Ware", 12.99];
-
-var bookArray = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11, book12];
-
+var bookArray = [book0, book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11];
