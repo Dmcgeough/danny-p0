a9ba0cfed6ed7a15d3db153b928cf1d4d0f89657
Dmcgeough <danny.mcgeough@gmail.com>
2018-06-21 15:44:58 -0400

    made express work.

--- javascript/express.js
+++ javascript/express.js
@@ -2,24 +2,24 @@ var express = require('express');
 var path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+app.get('', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
 });
 
 app.get('/online-store', (req, res) => {
-    res.sendFile(path.join(__dirname, './online-store.html'));
+    res.sendFile(path.join(__dirname, '../templates/online-store.html'));
 });
 
 app.get('/mission-statement', (req, res) => {
-    res.sendFile(path.join(__dirname, './mission-statement.html'));
+    res.sendFile(path.join(__dirname, '../templates/mission-statement.html'));
 });
 
 app.get('/shopping-cart', (req, res) => {
-    res.sendFile(path.join(__dirname, './shopping-cart.html'));
+    res.sendFile(path.join(__dirname, '../templates/shopping-cart.html'));
 });
 
 app.get('/contact-us', (req, res) => {
-    res.sendFile(path.join(__dirname, './contact-us.html'));
+    res.sendFile(path.join(__dirname, '../templates/contact-us.html'));
 });
 
 app.listen(5051);
