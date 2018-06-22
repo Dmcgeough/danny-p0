fa1d8c398f39865c907db8ce380545b37e2ccab6
Dmcgeough <danny.mcgeough@gmail.com>
2018-06-21 21:09:25 -0400

    More changes to functions.

--- javascript/express.js
+++ javascript/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 const app = express();
 
-//app.use(express.static('../styles/index.css'))
+app.use('/static', express.static('public'));
 
 app.get('', (req, res) => {
     res.sendFile(path.join(__dirname, '../templates/index.html'));
