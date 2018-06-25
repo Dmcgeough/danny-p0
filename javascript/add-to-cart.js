9e50d7dfa9d84def098b19478c8a4ab3ea30ffee
Dmcgeough <danny.mcgeough@gmail.com>
2018-06-25 12:05:01 -0400

    formatting changes

--- javascript/add-to-cart.js
+++ javascript/add-to-cart.js
@@ -14,17 +14,17 @@ function addToCart() {
 // cart page. The function to add up price would be fairly straight forward. However, the function that would require me to
 //pull values from an Array and then inject those values using JavaScript into a different HTML element is going to be tough.
 //var book = [title, price];
-var book0 = ["The Girl with the Dragon Tattoo", 7.99 ];
-var book1 = ["Never Go Back", 7.99];
-var book2 = ["Best Served Cold", 13.99];
-var book3 = ["Cronkite", 19.99];
-var book4 = ["50 Shades of Grey", 9.99];
-var book5 = ["The President is Missing", 17.99];
-var book6 = ["Shelter in Place", 14.99];
-var book7 = ["The Soul of America", 17.99];
-var book8 = ["The Fallen", 14.99];
-var book9 = ["Something in the Water", 13.99];
-var book10 = ["The Grey Ghost", 13.99];
-var book11 = ["The Death of Mrs. Westaway", 12.99];
+var book1 = ["The Girl with the Dragon Tattoo", 7.99 ];
+var book2 = ["Never Go Back", 7.99];
+var book3 = ["Best Served Cold", 13.99];
+var book4 = ["Cronkite", 19.99];
+var book5 = ["50 Shades of Grey", 9.99];
+var book6 = ["The President is Missing", 17.99];
+var book7 = ["Shelter in Place", 14.99];
+var book8 = ["The Soul of America", 17.99];
+var book9 = ["The Fallen", 14.99];
+var book10 = ["Something in the Water", 13.99];
+var book11 = ["The Grey Ghost", 13.99];
+var book12 = ["The Death of Mrs. Westaway", 12.99];
 
-var bookArray = [book0, book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11];
+var bookArray = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11, book12];
--- javascript/buy.js
+++ javascript/buy.js
@@ -4,11 +4,15 @@ addButton1.addEventListener('click', () => {
     document.querySelector("button[name='book1-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton2 = document.querySelector("button[name='book2-button']");
 
 addButton2.addEventListener('click', () => {  
     document.querySelector("button[name='book2-button']").innerHTML = "Added to Cart!";
 });
+
+
 
 var addButton3 = document.querySelector("button[name='book3-button']");
 
@@ -16,11 +20,15 @@ addButton3.addEventListener('click', () => {
     document.querySelector("button[name='book3-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton4 = document.querySelector("button[name='book4-button']");
 
 addButton4.addEventListener('click', () => {  
     document.querySelector("button[name='book4-button']").innerHTML = "Added to Cart!";
 });
+
+
 
 var addButton5 = document.querySelector("button[name='book5-button']");
 
@@ -28,11 +36,15 @@ addButton5.addEventListener('click', () => {
     document.querySelector("button[name='book5-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton6 = document.querySelector("button[name='book6-button']");
 
 addButton6.addEventListener('click', () => {  
     document.querySelector("button[name='book6-button']").innerHTML = "Added to Cart!";
 });
+
+
 
 var addButton7 = document.querySelector("button[name='book7-button']");
 
@@ -40,11 +52,15 @@ addButton7.addEventListener('click', () => {
     document.querySelector("button[name='book7-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton8 = document.querySelector("button[name='book8-button']");
 
 addButton8.addEventListener('click', () => {  
     document.querySelector("button[name='book8-button']").innerHTML = "Added to Cart!";
 });
+
+
 
 var addButton9 = document.querySelector("button[name='book9-button']");
 
@@ -52,17 +68,23 @@ addButton9.addEventListener('click', () => {
     document.querySelector("button[name='book9-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton10 = document.querySelector("button[name='book10-button']");
 
 addButton10.addEventListener('click', () => {  
     document.querySelector("button[name='book10-button']").innerHTML = "Added to Cart!";
 });
 
+
+
 var addButton11 = document.querySelector("button[name='book11-button']");
 
 addButton11.addEventListener('click', () => {  
     document.querySelector("button[name='book11-button']").innerHTML = "Added to Cart!";
 });
+
+
 
 var addButton12 = document.querySelector("button[name='book12-button']");
 
@@ -71,19 +93,7 @@ addButton12.addEventListener('click', () => {
 });
 
 
-
-
- 
-
-
 (function buyFunction() {
     'use strict'
     document.querySelector("#buy-button").innerHTML = "Added to Cart!";
 }());
-
-
-//maybe use the whole list and find a way to cycle through
-// var addButton1 = document.querySelector("button[name='stock1Add']");
-// this line will GRAB the button
-// addButton1.addEventListener('click', () => {   function code goes here }) ;
-// that line will DO SOMETHIGN when the button is CLICKED
